11a2946ccee6fe461b33aca62047f7622278dd8a
alkuzin <alkuzindev@gmail.com>
2024-03-29 10:29:35 +0300

    feat: added long list display

--- ls/ls.h
+++ ls/ls.h
@@ -6,18 +6,22 @@ UNIX projects
 #ifndef _UNIX_PROJECT_LS_H_
 #define _UNIX_PROJECT_LS_H_
 
+#include <sys/stat.h>
 #include <dirent.h>
 
 typedef struct ls_s {
+    char   filenames[256][256];
     struct dirent *dp;
     size_t size;
     DIR    *dir;
-    char filenames[256][256];
 } ls_t;
+
+typedef struct stat stat_t;
 
 void ls_init(ls_t *ls);
 void ls_opendir(ls_t *ls, const char *path);
 void ls_readdir(ls_t *ls);
+void ls_printdir_long_list_fmt(ls_t *ls);
 void ls_printdir(ls_t *ls);
 void ls_closedir(ls_t *ls);
 
