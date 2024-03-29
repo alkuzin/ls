cb8f33959b3f2b45954c581869c93c0dca08ede4
alkuzin <alkuzindev@gmail.com>
2024-03-29 13:01:28 +0300

    feat: added flags support

--- ls/ls.h
+++ ls/ls.h
@@ -8,6 +8,7 @@ UNIX projects
 
 #include <sys/stat.h>
 #include <dirent.h>
+#include <stdint.h>
 
 
 typedef struct ls_file_s {
@@ -22,13 +23,20 @@ typedef struct ls_file_s {
 
 typedef struct ls_s {
     ls_file_t files[256];
+    char    dirname[256];
     struct dirent *dp;
-    size_t size;
-    DIR    *dir;
+    size_t  size;
+    DIR     *dir;
+    uint8_t flags;
 } ls_t;
 
 typedef struct stat stat_t;
 
+#define LS_FLAG_LONG_LIST 0x01
+#define LS_FLAG_RECURCIVE 0x02
+#define LS_FLAG_ALL       0x04
+#define LS_FLAG_REVERSE   0x08
+#define LS_FLAG_TIME      0x10
 
 void ls_init(ls_t *ls);
 void ls_opendir(ls_t *ls, const char *path);
--- ls/main.c
+++ ls/main.c
@@ -6,28 +6,72 @@ UNIX projects
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "ls.h"
 
+void ls_help(void);
 
 int main(int argc, char **argv)
 {
+    char dir[256];
+    int  opt;
     ls_t ls;
 
     ls_init(&ls);
+    bzero(dir, sizeof(dir));
+    strncpy(dir, "./", 3);
 
+    if (argc > 1 && strncmp(argv[1], "--help", 6) == 0)
+        ls_help();
+
+    while ((opt = getopt(argc, argv, "lRart")) != -1) {
+        switch (opt) {
+            case 'l':
+                ls.flags |= LS_FLAG_LONG_LIST;
+                break;
+            case 'R':
+                ls.flags |= LS_FLAG_RECURCIVE;
+                break;
+            case 'a':
+                ls.flags |= LS_FLAG_ALL;
+                break;
+            case 'r':
+                ls.flags |= LS_FLAG_REVERSE;
+                break;
+            case 't':
+                ls.flags |= LS_FLAG_TIME;
+                break;
+            default:
+                ls_help();
+        }
+    }
+
+    if (!ls.flags && argc > 1)
+        strncpy(dir, argv[1], 256);
+    else if (argc > 1 && argv[argc - 1][0] != '-')
+        strncpy(dir, argv[argc - 1], 256);
+
+/*    printf("argc: %d\n", argc);
+    printf("argv[argc - 1]: \"%s\"\n", argv[argc - 1]);
+    printf("dir: \"%s\"\n", dir);
+*/
     /* open directory */
-    if (argc == 1)
-        ls_opendir(&ls, "./");
-    else if (argc == 2)
-        ls_opendir(&ls, argv[1]);
-
+    ls_opendir(&ls, dir);
+    
     /* read files from directory */
     ls_readdir(&ls);
 
-    ls_printdir(&ls);
+    ls_printdir_long_list_fmt(&ls);
 
     /* close directory */
     ls_closedir(&ls);
 
     return 0;
+
+}
+
+void ls_help(void)
+{
+    puts("Help ...");
+    exit(EXIT_SUCCESS);
 }
