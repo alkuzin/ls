31b4568b2644d4540db7a7de1da6b691d2a7418e
alkuzin <alkuzindev@gmail.com>
2024-03-29 13:04:53 +0300

    feat: modified output to the screen, added file size output adjustment

--- ls/ls.c
+++ ls/ls.c
@@ -52,40 +52,85 @@ void ls_get_mode(char *buffer, mode_t mode)
     buffer[9] = (S_IXOTH & mode) ? 'x' : '-';
 }
 
+static size_t ls_utoa_len(unsigned int n)
+{
+    size_t len;
+
+
+    len = 0;
+
+    if (n == 0)
+        return 1;
+
+    while (n >= 1)
+    {
+        len++;
+        n /= 10;
+    }
+
+    return len;
+}
+
+int ls_get_max_size_len(ls_t *ls)
+{
+    int    max_size_len;
+    ls_file_t *file;
+    size_t size;
+
+    max_size_len = 0;
+
+    for (size_t i = 0; i < ls->size; i++) {
+        file = &ls->files[i];
+        size = ls_utoa_len(file->size);
+    
+        if ((size_t) max_size_len < size)
+            max_size_len = size;
+    }
+
+    return max_size_len;
+}
+
 void ls_printdir_long_list_fmt(ls_t *ls)
 {
     ls_file_t *file;
+
+    int max_size_len;
+
+    max_size_len = ls_get_max_size_len(ls);
 
     for (size_t i = 0; i < ls->size; i++) {
         
         file = &ls->files[i];
 
-        printf("%s %lu %s %s %5lu %s %s\n", file->mode, file->nlink, file->user, 
-        file->group, file->size, file->date, file->filename);
+        printf("%s %2lu %s %s %*lu %-5s  %s\n", file->mode, file->nlink, file->user, 
+        file->group, max_size_len, file->size, file->date, file->filename);
     }
 }
 
 void ls_init(ls_t *ls)
 {
     bzero(ls, sizeof(ls_t));
-    ls->dir  = NULL;
-    ls->dp   = NULL;
-    ls->size = 0;
+    ls->dir   = NULL;
+    ls->dp    = NULL;
+    ls->size  = 0;
+    ls->flags = 0;
 }
 
 void ls_opendir(ls_t *ls, const char *path)
 {
     ls->dir = opendir(path);
 
+    strncpy(ls->dirname, path, 256);
+
     if (!ls->dir) {
-        perror("error to open directory");
+        printf("ls: cannot access \'%s\': %s\n", path, strerror(errno));
         exit(EXIT_FAILURE);
     }
 }
 
 void ls_readdir(ls_t *ls)
 {
-    char   *filename, *user, *group;
+    char   *user, *group;
     size_t size, nbtime;
     struct tm *mdate;
     int    status, i;
@@ -95,20 +140,24 @@ void ls_readdir(ls_t *ls)
 
     i = 0;
 
+    char filename[512];
+
     for (;;) {
         ls->dp = readdir(ls->dir);
         
         if (!ls->dp) {
             if (errno) {
-                perror("readdir");
+                perror("ls: readdir");
                 exit(EXIT_FAILURE);
             }
             break;
         }
 
-        filename = ls->dp->d_name;
+        bzero(filename, sizeof(filename));
+        strncpy(filename, ls->dirname, 256);
+        strncat(filename, ls->dp->d_name, 256);
 
-        if (strncmp(filename, ".", 1) == 0 || strncmp(filename, "..", 2) == 0) {
+        if (strncmp(ls->dp->d_name, ".", 1) == 0 || strncmp(ls->dp->d_name, "..", 2) == 0) {
             ls->size--;
             continue;
         }
@@ -116,7 +165,8 @@ void ls_readdir(ls_t *ls)
         status = lstat(filename, &sb);
 
         if (status == -1) {
-            perror("stat error");
+            printf("FILENAME: %s\n", filename);
+            perror("ls: stat error");
             exit(EXIT_FAILURE);
         }
 
@@ -127,7 +177,7 @@ void ls_readdir(ls_t *ls)
         mtime = sb.st_mtime;
         mdate = localtime(&mtime);
         
-        strncpy(file->filename, filename, strlen(filename));
+        strncpy(file->filename, ls->dp->d_name, strlen(ls->dp->d_name));
         strncpy(file->group, group, strlen(group));
         strncpy(file->user, user, strlen(user));
         ls_get_mode(file->mode, sb.st_mode);
@@ -230,7 +280,7 @@ void ls_closedir(ls_t *ls)
     status = closedir(ls->dir);
 
     if (status == -1) {
-        perror("error to close directory");
+        perror("ls: error to close directory");
         exit(EXIT_FAILURE);
     }
 }
